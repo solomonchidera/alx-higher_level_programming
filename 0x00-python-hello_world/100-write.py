e659a578ae6fe01cda02ef9fe90851f0f2158892
SolomonChidera <solochidera10@gmail.com>
2023-10-31 03:33:44 +0100

    Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-def main():
-    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-    sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
