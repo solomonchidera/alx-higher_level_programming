04038df65359abfc7ef1295cc217d3f3f8c1cd7b
SolomonChidera <solochidera10@gmail.com>
2023-11-07 04:17:25 +0100

    Add: task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        temp = my_list[0]
+        for i in my_list:
+            if i > temp:
+                temp = i
+        return temp
