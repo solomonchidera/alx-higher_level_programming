76085239b3a95998a10db02c136f836572da5893
Solomon Chidera Oyoke <116091273+SolomonChidera@users.noreply.github.com>
2023-11-21 04:58:46 +0000

    function that prints an integer with "{:d}".format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError):
-        pass
-    return isinstance(value, int)
+        return True
+    except (TypeError, ValueError):
+        return False
