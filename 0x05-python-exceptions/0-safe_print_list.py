8910466593feb0fe59c0991c14e540ef114e2ad5
Solomon Chidera Oyoke <116091273+SolomonChidera@users.noreply.github.com>
2023-11-21 04:59:39 +0000

    Update on task1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    while x > 0:
+    nbr_elmt_print = 0
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[count]), end="")
-            count += 1
-            x -= 1
-        except (IndexError, ValueError, TypeError):
-            pass
-        except Exception:
-            print("Catch all")
-        finally:
-            print("")
-    return count
+            print(my_list[i], end="")
+            nbr_elmt_print += 1
+        except IndexError:
+            break
+    print()
+    return nbr_elmt_print
