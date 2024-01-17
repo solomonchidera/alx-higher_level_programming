db8a82cb1257e8f0f2c1b5640f288b0f4e330df3
SolomonChidera <solochidera10@gmail.com>
2024-01-17 07:31:55 +0100

    #7 Show the description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,7 @@
 -- Show the details of a database
 USE hbtn_0c_0;
 
-SELECT COLUMN_NAME, DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- SELECT COLUMN_NAME, DATA_TYPE
+-- FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_NAME = 'first_table';
+DESC first_table;
