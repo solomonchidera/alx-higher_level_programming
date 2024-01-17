12544ab1625ac4291d64748263546a3e520d0369
SolomonChidera <solochidera10@gmail.com>
2024-01-17 06:50:59 +0100

    #6 Create a table for my database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a table with content
-CREATE TABLES IF NOT EXISTS first_table
-(id INT,
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
