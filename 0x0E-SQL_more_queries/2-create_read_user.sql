246b97882cea875a001471f12840a7aa9bf887d5
SolomonChidera <solochidera10@gmail.com>
2024-01-18 04:43:08 +0100

    script that creates the database hbtn_0d_2 and the user user_0d_2.

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+-- Create Database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create the user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant select privilege to user_0d_2 in the database hbtn_0d_2
+GRANT SELECT 
+ON hbtn_0d_2.* 
+TO 'user_0d_2'@'localhost';
