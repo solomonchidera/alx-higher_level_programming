c42fe25dd94a89cb825400e4f0d9358ffccacdf9
SolomonChidera <solochidera10@gmail.com>
2024-02-13 00:29:06 +0100

    Printing some sentences using loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let arrayOfString = ["C is fun", "Python is cool", "Javascript is amazing"];
-var i;
+const arrayOfString = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
 for (i = 1; i <= 1; i++) {
-    console.log(arrayOfString[0]);
-    console.log(arrayOfString[1]);
-    console.log(arrayOfString[2]);
+  console.log(arrayOfString[0]);
+  console.log(arrayOfString[1]);
+  console.log(arrayOfString[2]);
 }
