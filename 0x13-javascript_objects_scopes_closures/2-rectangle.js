8c5ca7681fd56591544e99589fa4b533f61f870c
SolomonChidera <solochidera10@gmail.com>
2024-02-14 22:08:01 +0100

    Class rectangle that creates an empty objecton encountering negative number

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        return {};
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
     }
     this.width = w;
     this.heigth = h;
   }
 }
-
 
 module.exports = Rectangle;
